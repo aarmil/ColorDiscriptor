bbda3e52abc3b3fb19a021a1f321c040df2832e6
Konstantin <kailinykh@gmail.com>
2021-02-24 23:03:43 +0500

    My First App: Learning how to use spinners and buttons

---
+++ app/src/main/java/com/demo/colordiscriptor/MainActivity.java
@@ -0,0 +1,34 @@
+package com.demo.colordiscriptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColors;
+    private TextView textViewDescriptionTemp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Получение ссылок на компоненты макета:
+        spinnerColors = findViewById(R.id.spinnerColors);
+        textViewDescriptionTemp = findViewById(R.id.textViewDescriptionTemp);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        textViewDescriptionTemp.setText(description);
+    }
+
+    private String getDescriptionByPosition(int position) {
+        String[] descriptions = getResources().getStringArray(R.array.description_of_temp);
+        return descriptions[position];
+    }
+}
